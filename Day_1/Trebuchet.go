8a7f34325192f3b826477004aa02b9750d405b52
Andres Guerrero Maldonado <42884620+Dredz223@users.noreply.github.com>
2025-01-14 22:55:32 -0800

    Update Trebuchet.go
    Using 1000 line input.

--- Day_1/Trebuchet.go
+++ Day_1/Trebuchet.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	file, ferr := os.Open("einput.txt")
+	file, ferr := os.Open("input.txt")
 	if ferr != nil {
 		panic(ferr)
 	}
@@ -17,31 +17,24 @@ func main() {
 	var sum int
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		num := ""
-		fmt.Println(num)
 		line := scanner.Text()
-		fmt.Printf("String # %v: %v\n", ctr, line)
-		re := regexp.MustCompile("[0-9]+")
+		re := regexp.MustCompile("[0-9]")
 		nums := re.FindAllString(line, -1)
 		ctr++
-		fmt.Printf("Nums %v\n", nums)
 		arrlen := len(nums)
-		fmt.Printf("Array Length: %v\n", arrlen)
-		fmt.Printf("First Value: %v\n", nums[0])
-		fmt.Printf("Second Value: %v\n", nums[arrlen-1])
 
 		//NumZero Tens place, NumOne Ones Place
 		numZero, err := strconv.Atoi(nums[0])
 		if err != nil {
 			fmt.Println("Error", err)
 		} else {
-			fmt.Printf("NumZero: %v\n", numZero)
+			// fmt.Printf("NumZero: %v\n", numZero)
 		}
 		numOne, err := strconv.Atoi(nums[arrlen-1])
 		if err != nil {
 			fmt.Println("Error", err)
 		} else {
-			fmt.Printf("NumOne: %v\n", numOne)
+			// fmt.Printf("NumOne: %v\n", numOne)
 		}
 		temp := numZero*10 + numOne
 		sum += temp
