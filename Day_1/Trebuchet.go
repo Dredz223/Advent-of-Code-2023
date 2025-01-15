7ecedbd8b874b29c22c6e98dafd57e8e70017826
Andres Guerrero Maldonado <42884620+Dredz223@users.noreply.github.com>
2025-01-14 22:44:44 -0800

    Solved
    Solve with Example text file.

--- Day_1/Trebuchet.go
+++ Day_1/Trebuchet.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 )
 
 func main() {
@@ -12,18 +13,38 @@ func main() {
 	if ferr != nil {
 		panic(ferr)
 	}
-	arry := make([][]int, 0, 0)
+	ctr := 1
+	var sum int
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		num := ""
 		fmt.Println(num)
 		line := scanner.Text()
-		fmt.Printf("Text file: %v\n", line)
-
+		fmt.Printf("String # %v: %v\n", ctr, line)
 		re := regexp.MustCompile("[0-9]+")
 		nums := re.FindAllString(line, -1)
+		ctr++
 		fmt.Printf("Nums %v\n", nums)
-	}
-	fmt.Println(arry)
+		arrlen := len(nums)
+		fmt.Printf("Array Length: %v\n", arrlen)
+		fmt.Printf("First Value: %v\n", nums[0])
+		fmt.Printf("Second Value: %v\n", nums[arrlen-1])
 
+		//NumZero Tens place, NumOne Ones Place
+		numZero, err := strconv.Atoi(nums[0])
+		if err != nil {
+			fmt.Println("Error", err)
+		} else {
+			fmt.Printf("NumZero: %v\n", numZero)
+		}
+		numOne, err := strconv.Atoi(nums[arrlen-1])
+		if err != nil {
+			fmt.Println("Error", err)
+		} else {
+			fmt.Printf("NumOne: %v\n", numOne)
+		}
+		temp := numZero*10 + numOne
+		sum += temp
+	}
+	fmt.Printf("Value %v", sum)
 }
